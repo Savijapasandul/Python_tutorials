8e68befb604ea9ddc473ac77e8aab5abf68aaf26
Savija pasandul <Pasanduls@gmail.com>
2024-05-24 13:47:27 +0100

    Variables and Concatenation

---
+++ concatenation_3/concatenation.py
@@ -0,0 +1,6 @@
+# conversation between me (Savija) and my boss
+print('Savija: Sorry Boss! I missed the first train so I ended up late.')
+print('Boss: Come on Savija! This is the second time this week. The project deadline is near.')
+print("Savija: I know boss. I'm sorry boss. I'll get up early tomorrow.")
+print("Boss: That's the same thing you told me yesterday.")
+
