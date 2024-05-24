c1de8d21753f3728686107175053b6bb9d0b962d
Savija pasandul <Pasanduls@gmail.com>
2024-05-24 13:01:08 +0100

    print

---
+++ printing_2/print.py
@@ -0,0 +1,8 @@
+# print('I was late.\nMy boss got mad.\nI had to work overtime.')
+# print('I was late.', end=' ')
+# print('My boss got mad.', end=' ')
+# print('I had to work overtime.', end=' ')
+
+print('''I was late.
+My boss got mad.
+I had to work overtime.''')
