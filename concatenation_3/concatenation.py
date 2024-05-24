e4d5f7e36a7fbc7df41230dc59cb3bdcb7d5e12a
Savija pasandul <Pasanduls@gmail.com>
2024-05-24 14:37:14 +0100

    new

--- concatenation_3/concatenation.py
+++ concatenation_3/concatenation.py
@@ -1,6 +1,16 @@
 # conversation between me (Savija) and my boss
-print('Savija: Sorry Boss! I missed the first train so I ended up late.')
-print('Boss: Come on Savija! This is the second time this week. The project deadline is near.')
-print("Savija: I know boss. I'm sorry boss. I'll get up early tomorrow.")
-print("Boss: That's the same thing you told me yesterday.")
+# print('Savija: Sorry Boss! I missed the first train so I ended up late.')
+# print('Boss: Come on Savija! This is the second time this week. The project deadline is near.')
+# print("Savija: I know boss. I'm sorry boss. I'll get up early tomorrow.")
+# print("Boss: That's the same thing you told me yesterday.")
 
+late = "Savija"
+mean = "Boss"
+
+print(late + ": Sorry Boss! I missed the first train so I ended up late." + " I'll make sure to wake up early tomorrow.")
+print(mean + ": Come on Savija! This is the second time this week. The project deadline is near.")
+print(late + ": I know boss. I'm sorry boss. I'll get up early tomorrow.")
+print(mean + ": That's the same thing you told me yesterday.")
+
+print(late, mean)
+print(late+mean)
