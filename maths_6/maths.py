3eb37bc45e8ce79c38107ce83d72cfbc5a5036a0
Savija pasandul <Pasanduls@gmail.com>
2024-05-29 15:16:43 +0100

    save

---
+++ maths_6/maths.py
@@ -0,0 +1,20 @@
+# A + B adds two numbers together
+# A - B subtracts the value B from A
+# A / B divides A by the value B
+# A * B multiplies A by b
+# A ** B raises A to the Bth power
+# A % B gives the remainder of the operation of dividing A by B
+
+# PEMDAS
+
+# (1) Parentheses
+# (2) Exponents next
+# (3) Multiplication, Division, and Modulus (%) are tied
+# (4) Addition and Subtraction are tied at the bottom
+
+x = 5 * 25
+y = 20/(50-35)*5
+
+print(x, y)
+print( type(x))
+print( type(y))
