16853b39f2f5283be346db87b9b68659454af845
Savija pasandul <Pasanduls@gmail.com>
2024-05-29 15:52:20 +0100

    new

---
+++ boolean_7/boolean.py
@@ -0,0 +1,11 @@
+# Boolean = True, False
+
+# A < B, A is less than B, True
+# A > B, A is greater than B, True 
+# A <= B, A is less than or equal to B, True
+# A >= B, A is greater than or equal to B, True
+# A == B, A is equal to B, True
+# A != B, A is not equal to B, True
+
+dogs_can_fly = False
+print(dogs_can_fly)
