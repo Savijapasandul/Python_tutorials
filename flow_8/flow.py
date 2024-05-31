b82a55948860846fe513001bc906118c34ccd01b
Savija pasandul <Pasanduls@gmail.com>
2024-05-31 09:45:40 +0100

    new

---
+++ flow_8/flow.py
@@ -0,0 +1,44 @@
+# Control Flow: python program flows from one line to the next and we can change or control by; if-statements, while-loops, and for-loops.
+
+# if statements - decision making
+
+# example 1
+code A
+if condition:
+    code B
+code C
+
+# example 2
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+# if statements - And / Or 
+# Basically a boolean expression is some form or comparison that yields either a True or a False
+
+# example 3
+height = int(input('Height in inches: '))
+if height >= 40:
+    if height <= 70: 
+        print('You may ride')
+if height < 40:
+    print('You may not ride')
+if height > 70:
+    print('You may not ride')
+
+# introducing else in if statements
+# example 2
+
+# Elif - more than two possible outcomes
+# # example 2
+
+# if/elif/else statements - make decisions in python
+
+
+
+
